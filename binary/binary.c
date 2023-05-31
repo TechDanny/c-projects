c14713b17f60a18f52d1f956a50e7bdf83e565cd
TechDanny <kokidanny15@gmail.com>
2023-05-31 09:52:13 -0400

    binary converter

---
+++ binary/binary.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * Create a program that converst a decimal number to
+ * binary number
+ */
+
+int main(void)
+{
+	int binary[32];
+	int i = 0;
+	int j;
+	int num;
+
+	printf("Enter a number: ");
+	scanf("%d", &num);
+
+	while (num > 0)
+	{
+		binary[i] = num % 2;
+		num = num / 2;
+		i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		printf("%d", binary[j]);
+
+	}
+	printf("\n");
+	return (0);
+}
